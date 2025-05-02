4a0960f5df22090f2ffd2adb98fdd82644060e0f
Eyal Hagai <eyalh2001@gmail.com>
2025-05-02 20:43:18 +0000

    advancing in node

--- api/node_deployments.go
+++ api/node_deployments.go
@@ -1,9 +1,13 @@
 package api
 
 import (
+	"encoding/base64"
+	"encoding/json"
 	"glider/containers"
 	"glider/workerpool"
 
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/image"
 	"github.com/docker/docker/client"
 	"github.com/gin-gonic/gin"
 )
@@ -17,6 +21,7 @@ type NodeDeployRequest struct {
 	DeploymentName string
 	Replicas       int
 	Image          string
+	Registry       string
 }
 
 func NewNodeDeploymentHandlers(workerPool *workerpool.WorkerPool, dockerCli *client.Client) NodeDeploymentHandlers {
@@ -26,6 +31,34 @@ func NewNodeDeploymentHandlers(workerPool *workerpool.WorkerPool, dockerCli *cli
 	}
 }
 
-func (h NodeDeploymentHandlers) Deploy(c *gin.Context, request NodeDeployRequest) (*containers.Container, error) {
+func (h NodeDeploymentHandlers) Deploy(c *gin.Context, request NodeDeployRequest) ([]containers.Container, error) {
+	// pull docker image from registry
+	auth := struct{}{}
+	authData, err := json.Marshal(auth)
+	if err != nil {
+		return nil, err
+	}
+	encodedAuth := base64.URLEncoding.EncodeToString(authData)
+
+	imageName := request.Registry + "/" + request.Image
+	imagePullOptions := image.PullOptions{
+		RegistryAuth: encodedAuth,
+	}
+
+	out, err := h.dockerCli.ImagePull(c, imageName, imagePullOptions)
+	if err != nil {
+		return nil, err
+	}
+	defer out.Close()
+
+	// create containers
+	for i := 0; i < request.Replicas; i++ {
+		resp, err := h.dockerCli.ContainerCreate(c, &container.Config{
+			Image: request.Image,
+		}, nil, nil, nil, request.DeploymentName+"-"+string(i+1))
+		if err != nil {
+			return nil, err
+		}
+	}
 	return nil, nil
 }
